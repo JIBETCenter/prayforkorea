0f569ec9298c81b4269bfb2a79ff2c8122ffc6d2
JIBETCenter <mathematics026@gmail.com>
2022-09-24 14:31:51 +0900

    2022년 9월 24일 setInterval을 사용한 페이지 단위 스크립트 작성

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,42 +1,96 @@
-window.onload = function () {
-    function resize() {
-        var $intro = document.getElementById("intro").offsetTop;
-        var $one = document.getElementById("one").offsetTop;
-        var $two = document.getElementById("two").offsetTop;
-        var $work = document.getElementById("work").offsetTop;
-        return [$intro, $one, $two, $work];
-    }
+var flag = false;
 
-    let sized = resize();
+function resize() {
+    var $intro = document.getElementById("intro").offsetTop;
+    var $one = document.getElementById("one").offsetTop;
+    var $two = document.getElementById("two").offsetTop;
+    var $work = document.getElementById("work").offsetTop;
+    return [$intro, $one, $two, $work];
+}
 
-    window.onresize = function () {
-        sized = resize();
-    };
+let sized = resize();
 
-    var aArr = document.querySelectorAll("a");
-
-    aArr.forEach((el) => {
-        let attr = el.getAttribute("data-go");
-        el.addEventListener("click", () => {
-            if (attr == "intro") return intro();
-            if (attr == "one") return one();
-            if (attr == "two") return two();
-            if (attr == "work") return work();
-        });
-    });
-
-    function intro() {
-        window.scrollTo({ top: sized[0], behavior: "smooth" });
-    }
-    function one() {
-        window.scrollTo({ top: sized[1], behavior: "smooth" });
-    }
-    function two() {
-        window.scrollTo({ top: sized[2], behavior: "smooth" });
-    }
-    function work() {
-        window.scrollTo({ top: sized[3], behavior: "smooth" });
-    }
-
-    if (window.onresize) return resize();
+window.onresize = function () {
+    sized = resize();
 };
+
+var aArr = document.querySelectorAll("a");
+
+aArr.forEach((el) => {
+    let attr = el.getAttribute("data-go");
+    el.addEventListener("click", () => {
+        if (attr == "intro") return intro();
+        if (attr == "one") return one();
+        if (attr == "two") return two();
+        if (attr == "work") return work();
+    });
+});
+
+function intro() {
+    window.scrollTo({ top: sized[0], behavior: "smooth" });
+}
+function one() {
+    window.scrollTo({ top: sized[1], behavior: "smooth" });
+}
+function two() {
+    window.scrollTo({ top: sized[2], behavior: "smooth" });
+}
+function work() {
+    window.scrollTo({ top: sized[3], behavior: "smooth" });
+}
+
+window.addEventListener(
+    "mousewheel",
+    (e) => {
+        e.preventDefault();
+        let sized = resize();
+        if (e.deltaY > 0 && flag === false) {
+            if (e.pageY < sized[1]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[1], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            } else if (e.pageY < sized[2]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[2], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            } else if (e.pageY < sized[3]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[3], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            }
+        } else if (e.deltaY < 0 && flag === false) {
+            if (e.pageY > sized[3]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[2], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            } else if (e.pageY > sized[2]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[1], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            } else if (e.pageY > sized[1]) {
+                flag = true;
+                setInterval(
+                    window.scrollTo({ top: sized[0], behavior: "smooth" }),
+                    1000
+                );
+                flag = false;
+            }
+        } else return;
+    },
+    { passive: false }
+);
